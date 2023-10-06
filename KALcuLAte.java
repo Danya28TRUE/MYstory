fea43f00111ec9afc068fb06c4fb8a92792939b3
DAN <147174620+Danya28TRUE@users.noreply.github.com>
2023-10-06 21:10:02 +0500

    Add files via upload
    калькулятор на java :)

---
+++ KALcuLAte.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class KALcuLAte {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        double number1 = scanner.nextDouble();
+        System.out.print("Введите второе число: ");
+        double number2 = scanner.nextDouble();
+        System.out.print("Введите оператор (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+        double result = 0;
+        switch (operator) {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                result = number1 / number2;
+                break;
+            default:
+                System.out.println("Некорректный оператор!");
+                return;
+        }
+        System.out.println("Результат: " + result);
+    }
+}
